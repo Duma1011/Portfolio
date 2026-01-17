87874b7fb7f759a009164034b0972ee4695e8b0a
Phumezile Tsitsa <tsitsap11@gmail.com>
2026-01-17 17:06:23 +0200

    updated the profile page to include preview image and text

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import {makeStyles,Text, Card,CardHeader,Body1} from "@fluentui/react-components";
+import {makeStyles,Text, Card,CardHeader,CardPreview,Body1,Image} from "@fluentui/react-components";
+import educationImg from '../assets/education.jpg';
+import experienceImg from '../assets/experience.jpg';
+import certificatesImg from '../assets/certfication.jpg';
+import toolsImg from '../assets/techtools.jpg';
 
 const useStyles = makeStyles({
   container: {
-    gap: "20px",
+    gap: "10px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    padding: "20px",
   },
 
   card: {
     width: "500px",
-    height: "300px",
+    height: "600px",
     padding: "10px",
-    margin: "auto",
     '::after': {
       borderTopColor: 'transparent !important',
       borderRightColor: 'transparent !important',
@@ -33,16 +37,26 @@ const useStyles = makeStyles({
     alignItems: "center",
   },
   cardHeader: {
-    paddingBottom: "0px",
-    borderBottom: "none",
+    fontSize: "20px",
+    fontWeight: "bold",
+    color:"#8E9A98"
   },
   text:{
         color:"#8E9A98"
     },
-    title:{
+    pageTitle:{
         fontSize: "20px",
         fontWeight: "bold",
         color:"#8E9A98"
+    },
+
+    professionDescription:{
+        padding: '20px',
+        maxWidth: '1000px',
+        color:"#8E9A98"
+    },
+    cardPreview:{
+        height: "250px",
     }
 });
 
@@ -51,11 +65,10 @@ const Profile=()=>{
     const styles = useStyles();
 
     return(
-
-     <> 
     <div className={styles.container}>
+            <Text className={styles.pageTitle}>My Profile</Text>
 
-            <Text style={{ padding: '20px',maxWidth: '1000px',color:"#8E9A98"}} >
+            <Text className={styles.professionDescription}>
                I am a Software Developer with proven ability to transform complex technical challenges into practical systems that drive business 
                impact. I am eager to leverage technical expertise and analytical thinking to build impactful solutions in a production environment, while building a strong software engineer foundation.
             </Text>
@@ -63,7 +76,10 @@ const Profile=()=>{
 
          <div className={styles.cardContainer}>
             <Card  className={styles.card} appearance="subtle"> 
-                <CardHeader header={<Body1 className={styles.title}>Education</Body1>}/>
+                <CardHeader header={<Body1 className={styles.cardHeader}>Education</Body1>}/>
+                <CardPreview className={styles.cardPreview}>
+                    <Image src={educationImg}/>
+                </CardPreview>
                 <ul className={styles.text}>
                     <li>BSc Mathematics and Computer Science - UCT(2019-2023)</li>
                     <li>NSC(Matric) - Bardale Secondary School(2018)</li>
@@ -72,7 +88,10 @@ const Profile=()=>{
             </Card>
   
            <Card  className={styles.card}>
-                <CardHeader header={<Body1 className={styles.title}>Experience</Body1>}/>
+                <CardHeader header={<Body1 className={styles.cardHeader}>Experience</Body1>}/>
+                <CardPreview className={styles.cardPreview}>
+                    <Image src={experienceImg}/>
+                </CardPreview>
                 <ul className={styles.text}>
                     <li>Feb 2025-Current: Graduate Software Developer, Advania UK</li>
                     <li>Nov 2024-Feb 2025: Artificial Intelligence (AI) Intern, Microsoft </li>
@@ -83,7 +102,10 @@ const Profile=()=>{
        
           <div className={styles.cardContainer}>
                <Card appearance="subtle" className={styles.card} > 
-                 <CardHeader header={<Body1 className={styles.title}>Certificates and Training</Body1>} className={styles.cardHeader}/>
+                 <CardHeader header={<Body1 className={styles.cardHeader}>Certificates and Training</Body1>} className={styles.cardHeader}/>
+                 <CardPreview className={styles.cardPreview}>
+                  <Image src={certificatesImg}/>
+                 </CardPreview>
                  <ul className={styles.text}>
                     <li>Developing Solutions for Microsoft Azure (AZ-204).</li>
                     <li>Azure AI Engineer Associate (AI-102).</li>
@@ -93,7 +115,10 @@ const Profile=()=>{
                </Card>
 
               <Card className={styles.card}>
-                  <CardHeader header={<Body1 className={styles.title}>Tools of trade</Body1>}/>
+                  <CardHeader header={<Body1 className={styles.cardHeader}>Tools of trade</Body1>}/>
+                  <CardPreview className={styles.cardPreview}>
+                    <Image src={toolsImg}/>
+                  </CardPreview>
                   <ul className={styles.text}>
                     <li>C#</li>
                     <li>Python </li>
@@ -106,21 +131,12 @@ const Profile=()=>{
                     <li>ASP.NET MVC</li>
                     <li>ASP.NET Core</li>
                     <li>Entity Framework</li>
+                    <li>SQL</li>
+                    <li>Azure</li>
                   </ul>
               </Card>
           </div>
-    </div>
-
-    <div>
-      <Card className={styles.card}>
-        <CardHeader header={<Body1>Cloud and Platforms</Body1>}/> 
-            <ul>
-              <li>Azure</li>
-              </ul>
-      </Card>
-    </div>
-  </>
-
+    </div> 
     );
 }
 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,8 +8,8 @@ import aiWebImg from '../assets/web_ai.jpg';
 const useStyles = makeStyles({
 card: {
     width: "500px",
-    height: "300px",
-    padding: "10px",
+    height: "700px",
+   // padding: "10px",
     margin: "auto",
     '::after': {
       borderTopColor: 'transparent !important',
@@ -21,7 +21,7 @@ card: {
   cardContainer:{
     display: "flex",
     flexDirection: "row", 
-    gap: "15px",
+    gap: "35px",
     alignItems: "center",
   },
    cardHeader:{
@@ -41,6 +41,12 @@ card: {
         fontWeight: "bold",
         color:"#8E9A98"
     },
+    projectImage:{
+        height: "250px",
+    },
+    projectDescription:{
+        color:"#8E9A98"
+    }
 });
 const Projects=()=>{
     const styles = useStyles();
@@ -50,24 +56,60 @@ const Projects=()=>{
         <div className={styles.cardContainer}>
             <Card className={styles.card}>
                 <CardHeader header={<Body1 className={styles.cardHeader}>SQL Assignments Automation</Body1>}/> 
-                <CardPreview><Image src={sqlAutomationImg}/></CardPreview>
+                <CardPreview className={styles.projectImage}><Image src={sqlAutomationImg} /></CardPreview>
+                <Text className={styles.projectDescription}>
+                    TESTSQL is a system to create and mark SQL assignments. It consists of a database of questions (data access requests in English) and answers (SQL SELECT statements to retrieve that data). The lecture will feed the system with question-pair and the assignment will then generate more based on the examples given. It generates a set of assignments, one for each student in the class, the assignment each student is given is derived from their student number. Each assignment consists of a fixed number of generated questions, spread equally across difficulty levels for each student. The system also stores in its database the data to be queried. Students can use the system to submit their assignment which is then marked automatically against a fixed expected output.
+                </Text>
+                <Text className={styles.projectDescription}>Tech Stack</Text>
+                <ul className={styles.projectDescription}>
+                    <li>Python</li>
+                    <li>SQL</li>
+                    <li>Tkinter</li>
+                </ul>
             </Card>
             <Card className={styles.card}>
                 <CardHeader header={<Body1 className={styles.cardHeader}>AI-WEB</Body1>}/> 
-                <CardPreview><Image src={aiWebImg}/></CardPreview>
+                <CardPreview className={styles.projectImage}><Image src={aiWebImg} className={styles.projectImage}/></CardPreview>
+                <Text className={styles.projectDescription}>
+                   AI-Web is a web application to explore and navigate the Artificial Intelligence research community in South Africa. The web application collects and organises data about South African Artificial Intelligence researchers from different publication websites across the internet such as NFR and web of science and with the data form a community of such researchers. Our application can provide answers to questions about the dominant topic within the community, publication venues where researchers in our community publish their papers, collaborations and the impact that our researchers have both locally and internationally, where the researchers are based.
+                   To develop our web application we coded using JavaScript, HTML and CSS. We used
+                   MongoDB, a database, to store all the AI South African researchers' data that we use to
+                   make queries. We made use of PowerBI to display the data distribution of the AI South
+                   African researchers.
+                </Text>
             </Card>
         </div>
 
         <div className={styles.cardContainer}>
             <Card className={styles.card} >
                 <CardHeader header={<Body1 className={styles.cardHeader}>Diabetes Prediction System</Body1>}/> 
-                <CardPreview><Image src={diebetespredictionImg}/></CardPreview>
+                <CardPreview className={styles.projectImage}><Image src={diebetespredictionImg}/></CardPreview>
+                <Text className={styles.projectDescription}>
+                    A diagnostic tool to predict whether a patient has diabetes based on certain diagnostic measurements included. To complete the task I had to use python machine learning libraries, such as numpy, pandas, skit-learn, matplotlib and seaborn, and django ,and html to develop an interactive user interface.
+                </Text>
+
+                <Text className={styles.projectDescription}> Tech Stack</Text>
+                <ul className={styles.projectDescription}>
+                    <li>Python</li>
+                    <li>Django</li>
+                    <li>Numpy</li>
+                    <li>Pandas</li>
+                    <li>Scikit-learn</li>
+                    <li>Seaborn</li>
+                    <li>Power BI</li>
+                </ul>
                 
             </Card>
             <Card className={styles.card}>
                 <CardHeader header={<Body1 className={styles.cardHeader}>Sales Data Analysis</Body1>}/> 
-                <CardPreview><Image src={salesAnalysisImage}/></CardPreview>
-                
+                <CardPreview className={styles.projectImage}><Image src={salesAnalysisImage}/></CardPreview>
+                <Text className={styles.projectDescription}>
+                    In this individual project, I utilised power BI and dived into a large sales dataset exploring sales trends over time, identifying the best-selling products, calculated revenue metrics such as total sales and profit margins, and create visualizations to present findings effectively.
+                </Text>
+                <Text className={styles.projectDescription}> Tech Stack</Text>
+                <ul className={styles.projectDescription}>
+                    <li>Power BI</li>
+                </ul>
             </Card>
         </div>
     
