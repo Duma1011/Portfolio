e1611de9daaa0b320877e8f1d0e463ce7957c610
Phumezile Tsitsa <tsitsap11@gmail.com>
2026-02-28 20:39:15 +0200

    removed reduntant styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ import { EmojiLaughRegular,BuildingRegular,RibbonStarRegular,ContactCardRegular}
 
 
 const useStyles=makeStyles({
-    container: {
+    mainPage: {
     gap: "10px",
     display: "flex",
     flexDirection: "row",
@@ -16,87 +16,15 @@ const useStyles=makeStyles({
     justifyContent: "center",
     width: "100%",
   },
-  cardContent:{
-    flexDirection: "row",
-    display: "flex",
-    height: "fit-content",
-    width: "fit-content",
-    gap: "20px",
-    padding: "10px",
-   // backgroundColor: "#414A4C",
-    
-  },
-  card:{
-    '::after': {
-      borderTopColor: 'transparent !important',
-      borderRightColor: 'transparent !important',
-      borderBottomColor: 'transparent !important',
-      borderLeftColor: 'transparent !important',
-      color:"#8E9A98"
-    }
-  },
-  cardContainer:{
-    padding: "20px",
-  },
-  personalInformationCard:{
-    alignItems:"center",
-    display: "flex",
-    flexDirection: "column",
-    gap: "25px",
-    paddingLeft: "200px",
-    '::after': {
-      borderTopColor: 'transparent !important',
-      borderRightColor: 'transparent !important',
-      borderBottomColor: 'transparent !important',
-      borderLeftColor: 'transparent !important',
-      color:"#8E9A98"
-    }
-  },
-  socialAccountCard:{
-    display: "flex",
-    flexDirection: "row",
-    gap: "20px",
-    '::after': {
-      borderTopColor: 'transparent !important',
-      borderRightColor: 'transparent !important',
-      borderBottomColor: 'transparent !important',
-      borderLeftColor: 'transparent !important',
-      color:"#8E9A98"
-    }
-  },
-  loggo:{
-    height: "35px",
-    width: "35px",
-  }, 
-    divider:{
-        height: '40px',
-        color: "#8E9A98"
-   },
-   cardLabel:{
-    color:"#F0F8FF",
-    fontSize: "16px",
-   },
-   cureerTitle:{
+   careerTitle:{
     color:"#00693E",
     fontSize:"40px", 
     fontWeight:"bold",
-    //SpaddingLeft:"20px"
    },
-   nameTitle:{
-     fontSize:"20px"
-    ,color:"#F0F8FF"
-   },
+
    IamLLabel:{
     color:"#8E9A98"
    }, 
-
-   titleSocialContainer:{
-    display: "flex",
-    alignItems: "flex-start",   
-    flexDirection: "column",
-    justifyContent: "space-between",
-    gap: "20px",
-   },
    frontPicture:{
     width: "300px",
     height:"300px",
@@ -104,32 +32,7 @@ const useStyles=makeStyles({
     boxShadow:"0 20px 40px rgba(0,0,0,0.4)",
   
    },
-   label:{
-    color:"#166534",
-    fontSize: "18px",
-    fontWeight: "bold"
-   },
-   buttonContainer:{
-    display:"flex",
-    flexDirection:"row",
-    '::after': {
-      borderTopColor: 'transparent !important',
-      borderRightColor: 'transparent !important',
-      borderBottomColor: 'transparent !important',
-      borderLeftColor: 'transparent !important',
-      color:"#8E9A98"
-    }
-   },
-   downloadButton:{
-      color:"white",
-      backgroundColor:"#166534",
-      width:"110px",
-      height: "30px",  
-      ":hover": { backgroundColor: "#14532d", color: "white" },
-      ":active": { backgroundColor: "#0f3d22", color: "white" },
-      ":visited": { color: "white" },
-   },
-   tab:{
+   tabListButton:{
     color:"#8E9A98",
     fontWeight:"500",
     ":hover":{
@@ -138,6 +41,7 @@ const useStyles=makeStyles({
 }
    
 });
+
 const Home=()=>{
     const styles=useStyles();
     const [selectedTab,setSelectedTab]=useState("home");
@@ -155,15 +59,13 @@ const Home=()=>{
       }
 
     return(
-     <div className={styles.container}>
-       {selectedTab === "home" &&  (  
-      
-               
-           <div style={{"paddingLeft":"120px","flexDirection": "column","display": "flex","gap":"30px","alignItems": "center"}}>
+     <div className={styles. mainPage}>
+       {selectedTab === "home" &&  (     
+        <div style={{"paddingLeft":"120px","flexDirection": "column","display": "flex","gap":"30px","alignItems": "center"}}>
             <Text >
                 <span className={styles.IamLLabel}>Hi, I am Phumezile<br /></span>
                  
-                 <span className={styles.cureerTitle}>Cloud and Software Engineer</span> 
+                 <span className={styles.careerTitle}>Cloud and Software Engineer</span> 
                 </Text>
            <Image
              src={profile}
@@ -175,23 +77,18 @@ const Home=()=>{
            <SearchBox size="large" style={{"backgroundColor":"white","borderRadius": "20px", "width": "400px" }}/>
 
            <TabList>
-              <Button onClick={handleAboutButton} className={styles.tab} icon={<EmojiLaughRegular />}>Me</Button>
-              <Button onClick={handleProjectButton} className={styles.tab} icon={<BuildingRegular />}>Projects </Button>
-              <Button onClick={handleProjectButton} className={styles.tab} icon={<RibbonStarRegular />}>Skills</Button>
-              <Button onClick={handleContactButton} className={styles.tab} icon={<ContactCardRegular />}>Contacts </Button>
-           </TabList>
+              <Button onClick={handleAboutButton} className={styles.tabListButton} icon={<EmojiLaughRegular />}>Me</Button>
+              <Button onClick={handleProjectButton} className={styles.tabListButton} icon={<BuildingRegular />}>Projects </Button>
+              <Button onClick={handleProjectButton} className={styles.tabListButton} icon={<RibbonStarRegular />}>Skills</Button>
+              <Button onClick={handleContactButton} className={styles.tabListButton} icon={<ContactCardRegular />}>Contacts </Button>
+        </TabList>
 
-          </div>
-        )}
+      </div>)
+    }
 
   {selectedTab==="about" && <Profile/>}
   {selectedTab==="projects" && <Projects/>}
   {selectedTab==="contact" && <Contact/>}
-
-</div>
-   
-    
-);
-}
+</div>);}
 
 export default Home;
